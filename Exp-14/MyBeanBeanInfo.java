04dd3e3707c4a3e7faf558de951b7c4f4cd67cc8
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2023-04-09 17:37:06 +0530

    Create MyBeanBeanInfo.java

---
+++ Exp-14/MyBeanBeanInfo.java
@@ -0,0 +1,17 @@
+package net.codejava.swing;
+
+import java.beans.*;
+
+public class MyBeanBeanInfo extends SimpleBeanInfo {
+	public PropertyDescriptor[] getPropertyDescriptors() {
+		try {
+			PropertyDescriptor nameDescriptor=new Propertydescriptor("name", MyBean.class);
+			nameDescriptor.setDisplayName("name");
+			return new PropertyDescriptor[] {nameDescriptor};
+		}
+		catch(IntrospectionException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
