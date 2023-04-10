e015641859e206c76715108c336337e7ba39881a
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2023-04-10 13:50:47 +0530

    Create SumServlet.java

---
+++ Exp-10/SumServlet.java
@@ -0,0 +1,38 @@
+package com.example.addition;
+
+import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SumServlet
+ */
+public class SumServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SumServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int num1=Integer.parseInt(request.getParameter("num1"));
+		int num2=Integer.parseInt(request.getParameter("num2"));
+		int sum=num1+num2;
+		PrintWriter out=response.getWriter();
+		out.println("Sum of the numbers is:"+" "+sum);
+	}
+
+}
