2dfdbe3d688ac4093abb336a1b5683c7a337ef5e
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2023-04-10 08:43:04 +0530

    Create RedirectServlet.java

---
+++ Exp-11/RedirectServlet.java
@@ -0,0 +1,34 @@
+package com.example.redirection;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class RedirectServlet
+ */
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RedirectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String website=request.getParameter("website");
+		response.sendRedirect(website);
+	}
+
+}
