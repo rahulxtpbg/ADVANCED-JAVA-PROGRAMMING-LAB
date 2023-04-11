d36e0a262d1ac2b53b370dee0f9ee521bf021878
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2023-04-11 09:56:04 +0530

    Updated MyFrame.java to Reflect Critical Changes

--- Exp-14/MyFrame.java
+++ Exp-14/MyFrame.java
@@ -1,7 +1,8 @@
 package net.codejava.swing;
 
 import java.awt.EventQueue;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,9 +12,30 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 
-public class MyFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
+	private MyBean myBean;
+	private JLabel label;
+	private JTextField textField;
+	
+	public MyFrame(){
+		myBean=new MyBean();
+		textField=new JTextField(10);
+		JButton button=new JButton("Set Property");
+		button.addActionListener(this);
+		label=new JLabel(myBean.getName());
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		getContentPane().add(button);
+		getContentPane().add(label);
+		getContentPane().add(textField);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		String newText = textField.getText();
+		myBean.setName(newText);
+		label.setText(myBean.getName());
+	}
 
 	/**
 	 * Launch the application.
@@ -35,29 +57,12 @@ public class MyFrame extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public MyFrame() {
-		MyBean myBean=new MyBean();
-		JTextField textField=new JTextField(10);
-		JButton button=new JButton("Set Property");
-		button.addActionListener(this);
-		JLabel label=new JLabel(myBean.getName());
-		getContentPane().add(button);
-		getContentPane().add(textField);
-		getContentPane().add(label);
-		
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		myBean.setName(((JTextField) getContentPane().getComponent(0)).getText());
-		label.setText(myBean.getName());
-		
 	}
 
 }
